4168b0d649a94e9eab7ff8d11465308b486fcbf3
BenHM10 <benhoffman.malau@gmail.com>
2022-04-04 13:41:38 +0700

    FeetIncToCm

---
+++ FeetIncToCm/src/test/java/com/rplbo/tocm/Main.java
@@ -0,0 +1,29 @@
+package com.rplbo.tocm;
+
+import java.io.*;
+
+    class convert {
+    static double FeetIncToCm(int Inches) {
+        double centimeter;
+        centimeter = 2.54 * Inches;
+        System.out.printf("Nilai dalam Centimeter: %.2f \n", centimeter);
+        return 0;
+    }
+
+        static double CmToFeetInch(int centimeter)
+        {
+            double inch, feet;
+            inch  = 0.3937 * centimeter;
+            feet = 0.0328 * centimeter;
+            System.out.printf("Inches is: %.2f", inch);
+            System.out.printf("Feet is: %.2f", feet);
+            return 0;
+        }
+
+        public static void main(String args[]) {
+        int Inches = 15;
+        int centimeter = 30;
+        FeetIncToCm(Inches);
+        CmToFeetInch(centimeter);
+    }
+}
